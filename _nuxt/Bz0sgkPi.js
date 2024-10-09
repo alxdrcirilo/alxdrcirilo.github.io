11dfc4beec287ee0b5a29e4e282c2aa04bbe8ffc
Alexandre Cirilo <38657258+alxdrcirilo@users.noreply.github.com>
2024-10-09 09:21:32 +0200

    dbc24c3

---
+++ _nuxt/Bz0sgkPi.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./Dnzdx-1w.js";import{l as o,o as n,U as s,w as f,W as l}from"./B53YTcrQ.js";const i=o({__name:"ProseA",props:{href:{type:String,default:""},target:{type:String,default:void 0,required:!1}},setup(e){return(t,u)=>{const r=a;return n(),s(r,{href:e.href,target:e.target},{default:f(()=>[l(t.$slots,"default")]),_:3},8,["href","target"])}}});export{i as default};
---
+++ _nuxt/D2-D5Eiu.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./NTCrlRZ7.js";import"./B53YTcrQ.js";import"./C-v3KzvZ.js";import"./Dnd51l0P.js";import"./OsN-lNhE.js";export{o as default};
