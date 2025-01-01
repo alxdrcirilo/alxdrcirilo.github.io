3ecd7a1bdf7ef396b4ec428b8312eb3c8c652816
Alexandre Cirilo <38657258+alxdrcirilo@users.noreply.github.com>
2025-01-01 08:42:20 +0000

    e990e4a

---
+++ _nuxt/BXuNxWYB.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./CUZawx5K.js";import"./CxMaD9l9.js";import"./C-v3KzvZ.js";import"./Dnd51l0P.js";import"./Dw9CyK2G.js";export{o as default};
---
+++ _nuxt/DIfXd2RK.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./BNq9iBtD.js";import{_ as r,c as n,a as s,b as c,o as a}from"./CxMaD9l9.js";import"./DC02AJqJ.js";import"./CFgg-CIX.js";import"./Dw9CyK2G.js";import"./CBhEeJNS.js";const p={};function _(i,o,m,l,d,f){const t=e;return a(),n("div",null,[o[0]||(o[0]=s("h1",null,"📔 Archive",-1)),c(t)])}const k=r(p,[["render",_]]);export{k as default};
